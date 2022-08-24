8d3910222871c767c79634ceea36b40d3caccd0b
Stefano d'Antonio <1582987+UnoSD@users.noreply.github.com>
2022-08-24 22:58:18 +0200

    Update setup-runner.sh

--- setup-runner.sh
+++ setup-runner.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mkdir actions-runner && cd actions-runner
 
 curl -o filename.tar.gz -L https://github.com/actions/runner/releases/download/v2.296.0/actions-runner-linux-x64-2.296.0-noexternals.tar.gz
@@ -5,5 +7,19 @@ curl -o filename.tar.gz -L https://github.com/actions/runner/releases/download/v
 tar xzf ./filename.tar.gz
 
 ./config.sh --unattended --url https://github.com/UnoSD/GitHubRunnerImageBuild --name $(hostname) --runnergroup Default --token <GITHUB REPO TOKEN> 
+
+./run.sh
+
+# Set up Azure DevOps agent
+
+cd ~
+
+mkdir myagent && cd myagent
+
+wget https://vstsagentpackage.azureedge.net/agent/2.206.1/vsts-agent-linux-x64-2.206.1.tar.gz
+
+tar zxf vsts-agent-linux-x64-2.206.1.tar.gz
+
+./config.sh --unattended --url <URL> --projectName <PROJECT> --acceptTeeEula --auth pat --token <DEVOPS PAT TOKEN> #--runAsService
 
 ./run.sh
