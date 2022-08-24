857599ac6b9b11c62ba904cf16af9ad61969980f
Stefano d'Antonio <1582987+UnoSD@users.noreply.github.com>
2022-08-24 21:43:14 +0200

    Update setup-runner.sh

--- setup-runner.sh
+++ setup-runner.sh
@@ -1,9 +1,9 @@
 mkdir actions-runner && cd actions-runner
 
-curl -o filename.tar.gz -L https://github.com/actions/runner/releases/download/v2.296.0/actions-runner-linux-x64-2.296.0-noruntime-noexternals.tar.gz
+curl -o filename.tar.gz -L https://github.com/actions/runner/releases/download/v2.296.0/actions-runner-linux-x64-2.296.0-noexternals.tar.gz
 
 tar xzf ./filename.tar.gz
 
-./config.sh --url https://github.com/UnoSD/GitHubRunnerImageBuild --token <GITHUB REPO TOKEN>
+./config.sh --unattended --url https://github.com/UnoSD/GitHubRunnerImageBuild --name $(hostname) --runnergroup Default --token <GITHUB REPO TOKEN> 
 
 ./run.sh
